c1242a50cdea0c737c090fba7f4e66ae1122a70f
Muphins <muphins42@gmail.com>
2021-02-10 10:08:59 +0100

    Replaced buttons by canvases.

--- i3touchdock.py
+++ i3touchdock.py
@@ -8,7 +8,10 @@ import os
 #############
 TD_W = 48
 TD_H = 20
+ICON_W = 48
+ICON_H = 48
 HOVBG='#303030'
+INCON_BG='black'
 dock = None
 i3=i3ipc.Connection()
 fpath='/usr/local/share/i3touchdock/res/'
@@ -17,69 +20,69 @@ isDockFloating = False
 #	Functions dock	#
 #####################
 #---- Close dock
-def mouseClickDockClose():
+def mouseClickDockClose(arg):
 	global dock
 #	print('close dock')
 	dock.destroy()
 	root.deiconify()
 
-def workspacePrev():
+def workspacePrev(arg):
 	wsNum = int(i3.get_tree().find_focused().workspace().name.split(':')[0])
 	if wsNum > 1:
 		wsNum -= 1
 		i3.command('workspace number ' + str(wsNum))
 
-def workspaceNext():
+def workspaceNext(arg):
 	wsNum = int(i3.get_tree().find_focused().workspace().name.split(':')[0])
 	if wsNum < 10:
 		wsNum += 1
 		i3.command('workspace number ' + str(wsNum))
 
-def windowToWorkspacePrev():
+def windowToWorkspacePrev(arg):
 	wsNum = int(i3.get_tree().find_focused().workspace().name.split(':')[0])
 	if wsNum > 1:
 		wsNum -= 1
 		i3.command('move to workspace number ' + str(wsNum))
 
-def windowToWorkspaceNext():
+def windowToWorkspaceNext(arg):
 	wsNum = int(i3.get_tree().find_focused().workspace().name.split(':')[0])
 	if wsNum < 10:
 		wsNum += 1
 		i3.command('move to workspace number ' + str(wsNum))
 
-def windowToUp():
+def windowToUp(arg):
 	i3.command('move up')
 
-def windowToDown():
+def windowToDown(arg):
 	i3.command('move down')
 
-def windowToLeft():
+def windowToLeft(arg):
 	i3.command('move left')
 
-def windowToRight():
+def windowToRight(arg):
 	i3.command('move right')
 
-def windowMaximizeToggle():
+def windowMaximizeToggle(arg):
 	i3.command('fullscreen toggle')
 
-def windowFloatingToggle():
+def windowFloatingToggle(arg):
 	i3.command('floating toggle')
 
 superKeyPress=False
-buttonWindowMove = None
-def windowMove():
+canvasWindowMove = None
+def windowMove(arg):
 	global superKeyPress
-	global buttonWindowMove
+	global canvasWindowMove
 	if superKeyPress:
 		superKeyPress=False
 		os.system('xdotool keyup Super_L')
-		buttonWindowMove.configure(bg='black', activebackground=HOVBG)
+		canvasWindowMove.configure(bg='black')
 	else:
 		superKeyPress=True
 		os.system('xdotool keydown Super_L')
-		buttonWindowMove.configure(bg='blue', activebackground='blue')
+		canvasWindowMove.configure(bg='blue')
 
-def windowClose():
+def windowClose(arg):
 	i3.command('kill')
 
 def dockCheckFullScreen(force=False):
@@ -129,27 +132,68 @@ def dockCheckFullScreen(force=False):
 def mouseClickRootEvent(arg):
 	print('open dock')
 	global dock
-	global buttonWindowMove
+	global canvasWindowMove
 	root.withdraw()
 	dock = tk.Toplevel(root, bg='black', borderwidth=0, highlightthickness=0)
 	dock.title("dock")
 	dock.geometry('800x48')
 	dockCheckFullScreen(force=True)
 	#dock.attributes('-type','dock')
-	buttonDockHide				= tk.Button(dock, bd=0, highlightthickness=0, bg='black', activebackground=HOVBG, image=png_dockHide				, command=mouseClickDockClose	).pack(side=tk.RIGHT)
-	buttonWorkspacePrev			= tk.Button(dock, bd=0, highlightthickness=0, bg='black', activebackground=HOVBG, image=png_workspacePrev			, command=workspacePrev			).pack(side=tk.LEFT)
-	buttonWorkspaceNext			= tk.Button(dock, bd=0, highlightthickness=0, bg='black', activebackground=HOVBG, image=png_workspaceNext			, command=workspaceNext			).pack(side=tk.LEFT)
-	buttonWindowToWorkspacePrev	= tk.Button(dock, bd=0, highlightthickness=0, bg='black', activebackground=HOVBG, image=png_windowToWorkspacePrev	, command=windowToWorkspacePrev	).pack(side=tk.LEFT)
-	buttonWindowToWorkspaceNext	= tk.Button(dock, bd=0, highlightthickness=0, bg='black', activebackground=HOVBG, image=png_windowToWorkspaceNext	, command=windowToWorkspaceNext	).pack(side=tk.LEFT)
-	buttonWindowToLeft			= tk.Button(dock, bd=0, highlightthickness=0, bg='black', activebackground=HOVBG, image=png_windowToLeft			, command=windowToLeft			).pack(side=tk.LEFT)
-	buttonWindowToUp			= tk.Button(dock, bd=0, highlightthickness=0, bg='black', activebackground=HOVBG, image=png_windowToUp				, command=windowToUp			).pack(side=tk.LEFT)
-	buttonWindowToDown			= tk.Button(dock, bd=0, highlightthickness=0, bg='black', activebackground=HOVBG, image=png_windowToDown			, command=windowToDown			).pack(side=tk.LEFT)
-	buttonWindowToRight			= tk.Button(dock, bd=0, highlightthickness=0, bg='black', activebackground=HOVBG, image=png_windowToRight			, command=windowToRight			).pack(side=tk.LEFT)
-	buttonWindowFloatingToggle	= tk.Button(dock, bd=0, highlightthickness=0, bg='black', activebackground=HOVBG, image=png_windowFloating			, command=windowFloatingToggle	).pack(side=tk.LEFT)
-	buttonWindowMaximizeToggle	= tk.Button(dock, bd=0, highlightthickness=0, bg='black', activebackground=HOVBG, image=png_windowMaximize			, command=windowMaximizeToggle	).pack(side=tk.LEFT)
-	buttonWindowMove			= tk.Button(dock, bd=0, highlightthickness=0, bg='black', activebackground=HOVBG, image=png_windowMove				, command=windowMove			)
-	buttonWindowMove.pack(side=tk.LEFT)
-	buttonWindowClose			= tk.Button(dock, bd=0, highlightthickness=0, bg='black', activebackground=HOVBG, image=png_windowClose				, command=windowClose			).pack(side=tk.LEFT)
+	canvasDockHide				= tk.Canvas(dock, bd=0, highlightthickness=0, bg=INCON_BG, width=ICON_W, height=ICON_H)
+	canvasWorkspacePrev			= tk.Canvas(dock, bd=0, highlightthickness=0, bg=INCON_BG, width=ICON_W, height=ICON_H)
+	canvasWorkspaceNext			= tk.Canvas(dock, bd=0, highlightthickness=0, bg=INCON_BG, width=ICON_W, height=ICON_H)
+	canvasWindowToWorkspacePrev	= tk.Canvas(dock, bd=0, highlightthickness=0, bg=INCON_BG, width=ICON_W, height=ICON_H)
+	canvasWindowToWorkspaceNext	= tk.Canvas(dock, bd=0, highlightthickness=0, bg=INCON_BG, width=ICON_W, height=ICON_H)
+	canvasWindowToLeft			= tk.Canvas(dock, bd=0, highlightthickness=0, bg=INCON_BG, width=ICON_W, height=ICON_H)
+	canvasWindowToUp			= tk.Canvas(dock, bd=0, highlightthickness=0, bg=INCON_BG, width=ICON_W, height=ICON_H)
+	canvasWindowToDown			= tk.Canvas(dock, bd=0, highlightthickness=0, bg=INCON_BG, width=ICON_W, height=ICON_H)
+	canvasWindowToRight			= tk.Canvas(dock, bd=0, highlightthickness=0, bg=INCON_BG, width=ICON_W, height=ICON_H)
+	canvasWindowFloatingToggle	= tk.Canvas(dock, bd=0, highlightthickness=0, bg=INCON_BG, width=ICON_W, height=ICON_H)
+	canvasWindowMaximizeToggle	= tk.Canvas(dock, bd=0, highlightthickness=0, bg=INCON_BG, width=ICON_W, height=ICON_H)
+	canvasWindowMove			= tk.Canvas(dock, bd=0, highlightthickness=0, bg=INCON_BG, width=ICON_W, height=ICON_H)
+	canvasWindowClose			= tk.Canvas(dock, bd=0, highlightthickness=0, bg=INCON_BG, width=ICON_W, height=ICON_H)
+	
+	canvasDockHide				.bind('<Button-1>', mouseClickDockClose		)
+	canvasWorkspacePrev			.bind('<Button-1>', workspacePrev			)
+	canvasWorkspaceNext			.bind('<Button-1>', workspaceNext			)
+	canvasWindowToWorkspacePrev	.bind('<Button-1>', windowToWorkspacePrev	)
+	canvasWindowToWorkspaceNext	.bind('<Button-1>', windowToWorkspaceNext	)
+	canvasWindowToLeft			.bind('<Button-1>', windowToLeft			)
+	canvasWindowToUp			.bind('<Button-1>', windowToUp				)
+	canvasWindowToDown			.bind('<Button-1>', windowToDown			)
+	canvasWindowToRight			.bind('<Button-1>', windowToRight			)
+	canvasWindowFloatingToggle	.bind('<Button-1>', windowFloatingToggle	)
+	canvasWindowMaximizeToggle	.bind('<Button-1>', windowMaximizeToggle	)
+	canvasWindowMove			.bind('<Button-1>', windowMove				)
+	canvasWindowClose			.bind('<Button-1>', windowClose				)
+	
+	canvasDockHide				.pack(side=tk.RIGHT)
+	canvasWorkspacePrev			.pack(side=tk.LEFT)
+	canvasWorkspaceNext			.pack(side=tk.LEFT)
+	canvasWindowToWorkspacePrev	.pack(side=tk.LEFT)
+	canvasWindowToWorkspaceNext	.pack(side=tk.LEFT)
+	canvasWindowToLeft			.pack(side=tk.LEFT)
+	canvasWindowToUp			.pack(side=tk.LEFT)
+	canvasWindowToDown			.pack(side=tk.LEFT)
+	canvasWindowToRight			.pack(side=tk.LEFT)
+	canvasWindowFloatingToggle	.pack(side=tk.LEFT)
+	canvasWindowMaximizeToggle	.pack(side=tk.LEFT)
+	canvasWindowMove			.pack(side=tk.LEFT)
+	canvasWindowClose			.pack(side=tk.LEFT)
+	
+	canvasDockHide				.create_image(ICON_W/2,ICON_H/2,image=png_dockHide				)
+	canvasWorkspacePrev			.create_image(ICON_W/2,ICON_H/2,image=png_workspacePrev			)
+	canvasWorkspaceNext			.create_image(ICON_W/2,ICON_H/2,image=png_workspaceNext			)
+	canvasWindowToWorkspacePrev	.create_image(ICON_W/2,ICON_H/2,image=png_windowToWorkspacePrev	)
+	canvasWindowToWorkspaceNext	.create_image(ICON_W/2,ICON_H/2,image=png_windowToWorkspaceNext	)
+	canvasWindowToLeft			.create_image(ICON_W/2,ICON_H/2,image=png_windowToLeft			)
+	canvasWindowToUp			.create_image(ICON_W/2,ICON_H/2,image=png_windowToUp			)
+	canvasWindowToDown			.create_image(ICON_W/2,ICON_H/2,image=png_windowToDown			)
+	canvasWindowToRight			.create_image(ICON_W/2,ICON_H/2,image=png_windowToRight			)
+	canvasWindowFloatingToggle	.create_image(ICON_W/2,ICON_H/2,image=png_windowFloating		)
+	canvasWindowMaximizeToggle	.create_image(ICON_W/2,ICON_H/2,image=png_windowMaximize		)
+	canvasWindowMove			.create_image(ICON_W/2,ICON_H/2,image=png_windowMove			)
+	canvasWindowClose			.create_image(ICON_W/2,ICON_H/2,image=png_windowClose			)
 
 #############
 #	Main	#
@@ -180,10 +224,10 @@ png_windowMove				= tk.PhotoImage(file = pngPath + "window-move.png")
 png_windowClose				= tk.PhotoImage(file = pngPath + "window-close.png")
 
 # display teardrop
-canvas = tk.Canvas(root, bg='black', width=TD_W, height=TD_H)
-canvas.config(highlightthickness=1)
-canvas.bind('<Button-1>', mouseClickRootEvent)
-canvas.pack()
-canvas.create_image(TD_W/2,TD_H/2,image=png_teardropDock)
+canvasTeardrop = tk.Canvas(root, bg=INCON_BG, width=TD_W, height=TD_H)
+canvasTeardrop.config(highlightthickness=1)
+canvasTeardrop.bind('<Button-1>', mouseClickRootEvent)
+canvasTeardrop.pack()
+canvasTeardrop.create_image(TD_W/2,TD_H/2,image=png_teardropDock)
 
 root.mainloop()
